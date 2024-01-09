58441c73c1b5ee0bc9872022de2cab63afe75d17
max177af <109582169+max177af@users.noreply.github.com>
2024-01-09 16:16:46 +0300

    Update task.py

--- task3/task.py
+++ task3/task.py
@@ -1,1 +1,26 @@
+import numpy as np
+from io import StringIO
 
+def task(csv_string):
+    csv_data = StringIO(csv_string)
+    data = np.genfromtxt(csv_data, delimiter=',', dtype=float)
+    n = len(data)-1
+    total_entropy = 0
+
+    for i in range(len(data)):
+        row_entropy = 0
+        for j in range(len(data[i])):
+            if not data[i][j]:
+                continue
+            row_entropy += data[i][j] /n * np.log2(data[i][j] / n)
+        total_entropy += -row_entropy
+
+    return total_entropy
+
+
+
+if __name__ == '__main__':
+    path = 'task3.csv'
+    with open(path, 'r') as file:
+        reader_csv = file.read()
+        task(reader_csv)
